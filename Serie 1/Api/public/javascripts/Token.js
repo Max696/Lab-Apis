74b935a90be1778a1c2cd088d290694b524fa67a
Max696 <maxferdiaz@gmail.com>
2018-11-26 21:55:40 -0600

    cleaning

--- Serie 1/Api/public/javascripts/Token.js
+++
@@ -1,7 +0,0 @@
-function GenerateToken(){
-    let mkey = document.getElementById('password').value;
-    let content = document.getElementById('jsonContent').value;
-
-    
-    document.location.href = "/content/"+ mkey +"&" + content;  
- }
